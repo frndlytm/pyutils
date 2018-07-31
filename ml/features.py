5e1283e86a0112f01245c96e4971069b7ff83ccc
Christian DiMare <frndlytm@gmail.com>
2018-07-31 10:10:00 -0400

    Simple doc formatting changes for future.

--- ml/features.py
+++ ml/features.py
@@ -48,19 +48,32 @@ def unstack_categories(data, keys=None, categories=None, features=None, sep='_')
     """unstack_categories takes some data and trades rows for columns for the
     given categories. For example, given the following data:
 
-        x | category | numeric1 | numeric2
-        1 | 1        | 50       | 100
-        1 | 2        | 10       | 20
-        2 | 1        | 20       | 40
-        3 | 1        | 20       | 40
-        3 | 2        | 50       | 100
+                +---+----------+----------+----------+
+                | x | category | numeric1 | numeric2 |
+                +===+==========+==========+==========+
+                | 1 | 1        | 50       | 100      |
+                +---+----------+----------+----------+
+                | 1 | 2        | 10       | 20       |
+                +---+----------+----------+----------+
+                | 2 | 1        | 20       | 40       |
+                +---+----------+----------+----------+
+                | 3 | 1        | 20       | 40       |
+                +---+----------+----------+----------+
+                | 3 | 2        | 50       | 100      |
+                +---+----------+----------+----------+
+
 
     Unstack transforms the DataFrame as follows
 
-        x | numeric1,1 | numeric1,2 | numeric2,1 | numeric2,2
-        1 | 50         | 10         | 100        | 20
-        2 | 20         | NaN        | 40         | NaN
-        3 | 20         | 50         | 40         | 100
+        +---+------------+------------+------------+------------+
+        | x | numeric1,1 | numeric1,2 | numeric2,1 | numeric2,2 |
+        +===+============+============+============+============+
+        | 1 | 50         | 10         | 100        | 20         |
+        +---+------------+------------+------------+------------+
+        | 2 | 20         | NaN        | 40         | NaN        |
+        +---+------------+------------+------------+------------+
+        | 3 | 20         | 50         | 40         | 100        |
+        +---+------------+------------+------------+------------+
 
     Unstacking features is useful when there is a category which causes duplication
     in the level you're trying to learn at.
@@ -74,11 +87,11 @@ def unstack_categories(data, keys=None, categories=None, features=None, sep='_')
     by one or the other.
 
     The function takes the parameters:
-        - data:         a pandas.DataFrame
-        - keys:         a list of look-up columns that are required.
-        - categories:   a list of categories to unstack.
-        - features:     a list of features to unstack by category.
-        - sep:          a separator when joining the feature name and category
+        * data:         a pandas.DataFrame
+        * keys:         a list of look-up columns that are required.
+        * categories:   a list of categories to unstack.
+        * features:     a list of features to unstack by category.
+        * sep:          a separator when joining the feature name and category
                             values into a list of new column headers.
     """
     if not keys:
